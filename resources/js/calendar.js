ad0824397e81436e2fb8bd655fea80451066c156
FlowNight <60231824+FlowNight@users.noreply.github.com>
2021-01-27 19:15:37 +0100

    Update

---
+++ resources/js/calendar.js
@@ -0,0 +1,48 @@
+window.onload = () => {
+    let elementCalendrier = document.getElementById("calendrier")
+
+    let xmlhttp = new XMLHttpRequest()
+
+    xmlhttp.onreadystatechange = () => {
+        if(xmlhttp.readyState == 4){
+            if(xmlhttp.status == 200){
+                let evenements = JSON.parse(xmlhttp.responseText)
+
+                // On instancie le calendrier
+                let calendrier = new FullCalendar.Calendar(elementCalendrier, {
+                    // On appelle les composants
+                    plugins: ['dayGrid','timeGrid','list','interaction'],
+                    defaultView: 'timeGridWeek',
+                    locale: 'fr',
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,list'
+                    },
+                    buttonText: {
+                        today: 'Aujourd\'hui',
+                        month: 'Mois',
+                        week: 'Semaine',
+                        list: 'Liste'
+                    },
+                    events: evenements,
+                    nowIndicator: true,
+                    editable: true,
+                    eventDrop: (infos) => {
+                        if(!confirm("Etes vous sûr.e de vouloir déplacer cet évènement")){
+                            infos.revert();
+                        }
+                    },
+                    eventResize: (infos) => {
+                        console.log(infos.event.end)
+                    }
+                })
+                calendrier.render()
+            }
+        }
+    }
+
+    xmlhttp.open('get', 'http://calendrier.test/liste.php', true)
+    xmlhttp.send(null)
+
+}
---
+++ resources/views/calendars.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Utilisation de FullCalendar</title>
+
+		<!--===== Fichiers CSS =====-->
+	
+        <link rel="stylesheet" href="../resources/fullcalendar/core/main.css">
+        <link rel="stylesheet" href="../resources/fullcalendar/daygrid/main.css">
+        <link rel="stylesheet" href="../resources/fullcalendar/timegrid/main.css">
+        <link rel="stylesheet" href="../resources/fullcalendar/list/main.css">
+        <link rel="stylesheet" href="../resources/css/calendar.css">
+    </head>
+    <body>
+        <div id="calendrier"></div>
+
+		<!--===== Fichiers JS =====-->
+        <script src="../resources/fullcalendar/core/main.js"></script>
+        <script src="../resources/fullcalendar/daygrid/main.js"></script>
+        <script src="../resources/fullcalendar/timegrid/main.js"></script>
+        <script src="../resources/fullcalendar/list/main.js"></script>
+        <script src="../resources/fullcalendar/interaction/main.js"></script>
+        <script src="../resources/js/calendar.js"></script>
+    </body>
+</html>
