0ee23fd6cf93ab9996ffbb47ce974c5eae401f41
FlowNight <60231824+FlowNight@users.noreply.github.com>
2021-01-27 18:10:22 +0100

    Update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('login');
 
 Route::get('/welcome.blade.php', function () {
     return view('welcome');
@@ -29,4 +30,4 @@ Route::get('/calendars.blade.php', function () {
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('logging');
+
